aed3c6a604bd09cb788089024603ffdad2458052
Alex Kovar <ajkovar@gmail.com>
2014-09-08 20:11:21 -0500

    added navigation for owned vs wanted

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,9 +16,23 @@ module.exports = React.createClass({
   },
 
   render: function() {
+
     return (
       <div className="container">
-        <h1>Xbox Voting</h1>
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">Xbox Voting</a>
+            </div>
+        
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li className={location.hash==="#/owned" ? "active" : undefined}><a href="#/owned">Owned</a></li>
+                <li className={location.hash==="#/wanted" ? "active" : undefined}><a href="#/wanted">Wanted</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <div className="text-danger">{this.state.error ? this.state.error : ""}</div>
         <this.props.activeRouteHandler/>
       </div>
